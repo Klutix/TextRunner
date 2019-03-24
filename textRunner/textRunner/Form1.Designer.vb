9ef45aabbe81b694267ebedc0e983fd19b4e633a
Gabriel Wohlford <GabrielWohlford@gmail.com>
2019-03-24 04:39:02 -0600

    Added TextRunner

---
+++ textRunner/textRunner/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_textRunner
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.GameLoopTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.txtHiddenControl = New System.Windows.Forms.TextBox()
+        Me.GameOvar = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Constantia", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(234, 12)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical
+        Me.RichTextBox1.Size = New System.Drawing.Size(639, 564)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(494, 582)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 1
+        Me.btnStart.Text = "START"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'GameLoopTimer
+        '
+        Me.GameLoopTimer.Interval = 200
+        '
+        'txtHiddenControl
+        '
+        Me.txtHiddenControl.Location = New System.Drawing.Point(1199, 12)
+        Me.txtHiddenControl.Name = "txtHiddenControl"
+        Me.txtHiddenControl.Size = New System.Drawing.Size(100, 20)
+        Me.txtHiddenControl.TabIndex = 2
+        '
+        'GameOvar
+        '
+        Me.GameOvar.Interval = 500
+        '
+        'frm_textRunner
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1402, 609)
+        Me.Controls.Add(Me.txtHiddenControl)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Name = "frm_textRunner"
+        Me.Text = "TextRunner"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+    Friend WithEvents GameLoopTimer As System.Windows.Forms.Timer
+    Friend WithEvents txtHiddenControl As System.Windows.Forms.TextBox
+    Friend WithEvents GameOvar As System.Windows.Forms.Timer
+
+End Class
---
+++ textRunner/textRunner/Form1.vb
@@ -0,0 +1,169 @@
+﻿Public Class frm_textRunner
+    Dim strInLine As String
+    Dim currentLine As Integer = 0
+    Dim LastLine As Integer
+    Dim player As String = "v"
+    Dim map() As String
+    Dim direction As Integer = 0
+    Dim blnCollision As Boolean
+    Dim isrunning As Boolean = False
+    Dim arGameOvar() As String
+    Dim strGAMEOVER As String
+    Dim newRound As Boolean
+    Dim gameOerSwitchTic As Integer = 0
+
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStart.Click
+        newRound = True
+        isrunning = True
+        GameOvar.Enabled = False
+        strInLine = ""
+        strInLine = My.Computer.FileSystem.ReadAllText("E:\p.txt")
+        strGAMEOVER = My.Computer.FileSystem.ReadAllText("E:\GAMEOVER.txt")
+
+
+        RichTextBox1.Text = strInLine
+        map = Split(strInLine, "|")
+        GameLoopTimer.Enabled = True
+
+
+    End Sub
+
+
+    Public Function insertV(ByRef location As Integer) As Boolean
+        If (currentLine < UBound(map)) Then
+            If (map(location).Substring(12 + direction, 1) = " ") Or newRound = True Then
+
+
+                map(location) = map(location).Remove(12 + direction, 1)
+                map(location) = map(location).Insert(12 + direction, player)
+
+                'remove a space to keep things looking even
+                'map(location) = map(location).Remove(13, 2)
+
+                RichTextBox1.SelectionStart = currentLine
+                newRound = False
+            Else
+                GameLoopTimer.Enabled = False
+
+
+                Return False
+                Exit Function
+
+            End If
+        End If
+
+        Return True
+
+    End Function
+
+    Public Sub buildString()
+        strInLine = ""
+        For x = currentLine - 1 To UBound(map)
+            strInLine += map(x)
+        Next
+    End Sub
+
+    'Public Sub removeV(ByRef lastLne As Integer)
+    '    If (currentLine < UBound(map)) Then
+    '        If map(lastLne).Contains(",") Then
+    '            map(lastLne) = map(lastLne).Replace("v", " ")
+
+    '            'insert a space to keep things looking even
+    '            map(lastLne) = map(lastLne).Insert(13 + direction, " ")
+    '        End If
+    '    End If
+    'End Sub
+
+
+
+
+
+    Private Sub GameLoopTimer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GameLoopTimer.Tick
+
+
+        If (isrunning = True) Then
+
+            '1 check user user input
+            txtHiddenControl.Focus()
+
+            '2.insert v into string 
+            If (insertV(currentLine)) Then
+                currentLine += 1
+                LastLine = currentLine - 1
+            Else
+                gameOver()
+                Exit Sub
+            End If
+            '3.rebuild string
+            buildString()
+
+
+
+            '3.redraw
+            RichTextBox1.Text = strInLine
+            RichTextBox1.Lines(currentLine + 3) += RichTextBox1.Lines(currentLine).Insert(12 + direction, player)
+
+
+        End If
+
+
+    End Sub
+
+
+
+
+
+    Private Sub TextBox1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txtHiddenControl.KeyDown
+        Select Case e.KeyCode
+            Case Keys.NumPad4
+
+                direction += -1
+
+            Case Keys.NumPad6
+                direction += 1
+        End Select
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        txtHiddenControl.Focus()
+
+    End Sub
+
+    Public Sub gameOver()
+        GameLoopTimer.Enabled = False
+        isrunning = False
+
+        RichTextBox1.Text = strGAMEOVER
+        arGameOvar = Split(strGAMEOVER, "|")
+
+
+
+        GameOvar.Enabled = True
+    End Sub
+
+    Private Sub GameOvar_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GameOvar.Tick
+
+
+
+
+        gameOver()
+        rebuildGAMEOVERstr()
+    End Sub
+
+    Public Sub rebuildGAMEOVERstr()
+        Select Case gameOerSwitchTic
+            Case 1
+                strGAMEOVER = strGAMEOVER.Replace("GAME", "OVER")
+                gameOerSwitchTic = 0
+            Case 0
+                strGAMEOVER = strGAMEOVER.Replace("OVER", "GAME")
+                gameOerSwitchTic = 1
+        End Select
+
+
+    End Sub
+
+End Class
